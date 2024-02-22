facd89223627d4341340cc9cb38879e3779fe9df
Johamm Castro <137451327+JohammCastro@users.noreply.github.com>
2024-02-22 17:48:23 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 // const port=process.env.PORT || 3000;
 
 // settings
-app.set('port',process.env.PORT || 3000)
+app.set('port',process.env.PORT || 4000)
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
